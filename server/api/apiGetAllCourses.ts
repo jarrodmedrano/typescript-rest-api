1580c4b8b7f2d9da6818640dfa5496042b81e2f1
Jarrod Medrano <jarrodmedrano@Jarrods-MacBook.local>
2017-08-02 10:14:40 -0700

    moving error handler

--- server/api/apiGetAllCourses.ts
+++ server/api/apiGetAllCourses.ts
@@ -1,6 +1,8 @@
 import {Request, Response} from 'express';
 import {findAllCourses} from '../queries/findAllCourses';
 import * as _ from 'lodash';
+import {onError} from './onError';
+
 export function apiGetAllCourses(req:Request, res:Response) {
 
         findAllCourses()
@@ -10,9 +12,4 @@ export function apiGetAllCourses(req:Request, res:Response) {
             })
             .catch(_.partial(onError, res, "Find all courses failed"));
 
-}
-
-function onError(res: Response, message: string, err: any) {
-    console.error('Promise chain error', message, err);
-    res.status(500).send();
 }
