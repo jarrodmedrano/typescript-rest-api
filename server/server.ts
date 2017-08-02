724a13f66d6ec89ad1f7189cdfb024172b0d4774
Jarrod Medrano <jarrodmedrano@Jarrods-MacBook.local>
2017-08-02 09:49:59 -0700

    add errorHandling

--- server/server.ts
+++ server/server.ts
@@ -3,14 +3,13 @@ import {findAllCourses} from './queries/findAllCourses';
 import * as express from 'express';
 import {Application} from 'express';
 import {initRestApi} from './api/api';
+import {apiErrorHandler} from './api/apiErrorHandler';
 
 const app: Application = express();
 
 initRestApi(app);
 
-app.use((err, req, res) => {
-    console.log('middleware executed');
-});
+app.use(apiErrorHandler);
 
 app.listen(8090, () => {
     console.log('server is running on port 8090...');
