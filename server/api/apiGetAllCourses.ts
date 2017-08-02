5aa897ebdbd89e4b4fe32a5c61bb177fc493667a
Jarrod Medrano <jarrodmedrano@Jarrods-MacBook.local>
2017-08-02 10:11:11 -0700

    adding partial error

--- server/api/apiGetAllCourses.ts
+++ server/api/apiGetAllCourses.ts
@@ -1,15 +1,18 @@
 import {Request, Response} from 'express';
 import {findAllCourses} from '../queries/findAllCourses';
-
+import * as _ from 'lodash';
 export function apiGetAllCourses(req:Request, res:Response) {
 
         findAllCourses()
+            .then(() => {throw new Error('Error')})
             .then(results => {
                 res.status(200).json({results})
-        });
-            
-        //throw new Error('Error occured');
-        
-        
+            })
+            .catch(_.partial(onError, res, "Find all courses failed"));
 
+}
+
+function onError(res: Response, message: string, err: any) {
+    console.error('Promise chain error', message, err);
+    res.status(500).send();
 }
