326a7fb888fd35a5ae843dd6d5640924cfec1204
Jarrod Medrano <jarrodmedrano@Jarrods-MacBook.local>
2017-08-02 10:21:31 -0700

    adding onSuccess handler

--- server/api/apiGetAllCourses.ts
+++ server/api/apiGetAllCourses.ts
@@ -2,14 +2,12 @@ import {Request, Response} from 'express';
 import {findAllCourses} from '../queries/findAllCourses';
 import * as _ from 'lodash';
 import {onError} from './onError';
+import {onSuccess} from './onSuccess';
 
 export function apiGetAllCourses(req:Request, res:Response) {
 
         findAllCourses()
-            .then(() => {throw new Error('Error')})
-            .then(results => {
-                res.status(200).json({results})
-            })
+            .then(_.partial(onSuccess, res))
             .catch(_.partial(onError, res, "Find all courses failed"));
 
 }
