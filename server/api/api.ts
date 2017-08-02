c3da42e7ca05884c541aad257ff01542c826c075
Jarrod Medrano <jarrodmedrano@Jarrods-MacBook.local>
2017-08-02 09:20:07 -0700

    moving getAllCourses api route

--- server/api/api.ts
+++ server/api/api.ts
@@ -1,5 +1,8 @@
 import {Application} from 'express';
+import {findAllCourses} from '../queries/findAllCourses';
+import {apiGetAllCourses} from './apiGetAllCourses';
 
-export function initRestApi() {
+export function initRestApi(app:Application) {
 
+    app.route('/api/courses').get(apiGetAllCourses);
 }
--- server/server.ts
+++ server/server.ts
@@ -4,16 +4,10 @@ import * as express from 'express';
 import {Application} from 'express';
 import {initRestApi} from './api/api';
 
-const app = express();
+const app: Application = express();
 
-app.route('/api/courses').get((req, res) => {
+initRestApi(app);
 
-    findAllCourses()
-        .then(results => {
-            res.status(200).json({results})
-    });
-
-});
 
 app.listen(8090, () => {
     console.log('server is running on port 8090...');
