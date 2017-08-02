d3c24510f8a76c2653b6c2e02eb7151be60e740b
Jarrod Medrano <jarrodmedrano@Jarrods-MacBook.local>
2017-08-02 09:07:12 -0700

    starting api design

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,8 @@
 
 import {findAllCourses} from './queries/findAllCourses';
 import * as express from 'express';
+import {Application} from 'express';
+import {initRestApi} from './api/api';
 
 const app = express();
 
@@ -8,11 +10,11 @@ app.route('/api/courses').get((req, res) => {
 
     findAllCourses()
         .then(results => {
-            res.status(200).json(results)
+            res.status(200).json({results})
     });
 
 });
 
 app.listen(8090, () => {
-    console.log('server is running...');
+    console.log('server is running on port 8090...');
 });
